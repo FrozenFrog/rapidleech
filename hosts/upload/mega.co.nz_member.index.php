d7a31c15b9201e102b6a969fb3bf9f4e516d0241
FrozenFrog <badauyeu4180@gmail.com>
2015-08-22 00:27:28 +0700

    Remove unused plugins

--- hosts/upload/mega.co.nz_member.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[] = 'mega.co.nz_member';
-$max_file_size['mega.co.nz_member'] = 2048;
-$page_upload['mega.co.nz_member'] = 'mega.co.nz_member.php';
-?>
--- hosts/upload/upfile.mobi.index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$upload_services[] = 'upfile.mobi';
-$max_file_size['upfile.mobi'] = 1024; // Filesize limit (MB)
-$page_upload['upfile.mobi'] = 'upfile.mobi.php';
-?>
